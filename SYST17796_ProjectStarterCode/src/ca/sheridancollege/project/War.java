8c378ca3fe04cb042af3d7f01c994902a93c5823
yousef El-khadray <elkhadra@sheridancollege.ca>
2021-10-28 17:28:53 -0400

    Third Commit

---
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/War.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author yousef
+ */
+public class  War  {
+     
+      public GroupOfCards groupOfCards2 = new GroupOfCards();
+}
+
